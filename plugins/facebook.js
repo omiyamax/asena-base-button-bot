0d39dc03928f766fe5c3fe92a7f87546f1ddcb17
Omindu Anjana <93399269+OminduAnjane@users.noreply.github.com>
2022-10-14 08:05:15 +0530

    Update facebook.js

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -21,7 +21,7 @@ Aqua.addCommand({ pattern: 'fb ?(.*)', fromMe: wk, desc:Lang.FB_DESC, deleteComm
     var up= await message.client.sendMessage(message.jid,Lang.FB_UP, MessageType.text, { quoted: message.data });
     await message.client.deleteMessage(message.jid, {id: load.key.id, remoteJid: message.jid, fromMe: true}) ; 
     var msg = ''
-     if (Config.DETAILS == 'true') msg = '┌───[🐋𝙰𝚀𝚄𝙰𝙱𝙾𝚃🐋]\n\n  *📥FACEBOOK DOWNLODER*\n\n│🎪ᴛɪᴛʟᴇ: ' + res3.data.title + '\n\n└───────────◉'
+     if (Config.DETAILS == 'true') msg = '┌───[👑𝙺𝙸𝙽𝙶 𝚁𝙰𝚆𝙰𝙽𝙰👑]\n\n  *📥FACEBOOK DOWNLODER*\n\n│🎪ᴛɪᴛʟᴇ: ' + res3.data.title + '\n\n└───────────◉'
      if (Config.DETAILS == 'false') msg = Config.CAPTION       
     const viddata = await axios.get(res3.data.videoUrl, { responseType: 'arraybuffer'}); 
     await message.sendMessage(Buffer.from(viddata.data), MessageType.video, { caption: msg, quoted: message.data}); 
@@ -34,7 +34,7 @@ Aqua.addCommand({ pattern: 'fb ?(.*)', fromMe: wk, desc:Lang.FB_DESC, deleteComm
            var up= await message.client.sendMessage(message.jid,Lang.FB_UP, MessageType.text, { quoted: message.data });
       await message.client.deleteMessage(message.jid, {id: load.key.id, remoteJid: message.jid, fromMe: true}) ; 
     var msg = ''
-      if (Config.DETAILS == 'true') msg = '┌───[🐋𝙰𝚀𝚄𝙰𝙱𝙾𝚃🐋]\n\n  *📥FACEBOOK DOWNLODER*\n\n│🎭ᴜᴘʟᴏᴀᴅᴇʀ: ' + res.data.result.author + '\n\n│🎪ᴛɪᴛʟᴇ: ' + res.data.result.title + '\n\n└───────────◉'
+      if (Config.DETAILS == 'true') msg = '┌───[👑𝙺𝙸𝙽𝙶 𝚁𝙰𝚆𝙰𝙽𝙰👑]\n\n  *📥FACEBOOK DOWNLODER*\n\n│👩‍💻ᴜᴘʟᴏᴀᴅᴇʀ: ' + res.data.result.author + '\n\n│🎪ᴛɪᴛʟᴇ: ' + res.data.result.title + '\n\n└───────────◉'
       if (Config.DETAILS == 'false') msg = Config.CAPTION       
      const viddata = await axios.get(res.data.result.HD_URL, { responseType: 'arraybuffer'}); 
     await message.sendMessage(Buffer.from(viddata.data), MessageType.video, { caption: msg, quoted: message.data}); 
@@ -46,7 +46,7 @@ Aqua.addCommand({ pattern: 'fb ?(.*)', fromMe: wk, desc:Lang.FB_DESC, deleteComm
      var up= await message.client.sendMessage(message.jid,Lang.FB_UP, MessageType.text, { quoted: message.data });
       await message.client.deleteMessage(message.jid, {id: load.key.id, remoteJid: message.jid, fromMe: true}) ; 
      var msg = ''
-      if (Config.DETAILS == 'true')  msg = '┌───[🐋𝙰𝚀𝚄𝙰𝙱𝙾𝚃🐋]\n\n  *📥FACEBOOK DOWNLODER*\n\n│🎪ᴛɪᴛʟᴇ: ' + response.data.title + '\n\n└───────────◉'
+      if (Config.DETAILS == 'true')  msg = '┌───[👑𝙺𝙸𝙽𝙶 𝚁𝙰𝚆𝙰𝙽𝙰👑]\n\n  *📥FACEBOOK DOWNLODER*\n\n│🎪ᴛɪᴛʟᴇ: ' + response.data.title + '\n\n└───────────◉'
       if (Config.DETAILS == 'false') msg = Config.CAPTION  
      const viddata = await axios.get(response.data.result[0].url, { responseType: 'arraybuffer'}); 
     await message.sendMessage(Buffer.from(viddata.data), MessageType.video, { caption: msg, quoted: message.data}); 
